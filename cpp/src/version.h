0a7cff3124fa67f6848a14b57f0f57d0f6354cb0
liquid-wq <liqui@mailbox.org>
2026-07-15 18:48:38 +0000

    Add files via upload

--- cpp/src/version.h
+++ cpp/src/version.h
@@ -4,4 +4,4 @@
 // Oeffentliche Versionsnummer (Semantic Versioning). Wird unabhaengig von
 // MEGA_RAW_CPP_BUILD hochgezaehlt - Build ist der interne, technische
 // Zaehler fuers Update-System, Version die menschenlesbare Release-Nummer.
-#define MEGA_RAW_VERSION "1.0.0"
+#define MEGA_RAW_VERSION "1.0.1"
